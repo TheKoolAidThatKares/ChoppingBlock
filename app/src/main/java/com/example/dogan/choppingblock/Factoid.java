a427c0fa47947dae228f8e0687bc8d0885dbf3b6
TheKoolAidThatKares <doganervin@gmail.com>
2018-04-24 05:04:39 -0500

    Added the Factoid and Coin class

--- app/src/main/java/com/example/dogan/choppingblock/Factoid.java
+++ app/src/main/java/com/example/dogan/choppingblock/Factoid.java
@@ -7,7 +7,9 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class Factoid {
 
@@ -48,8 +50,43 @@ public class Factoid {
 
                     for(String id : coins)
                     {
+                        String nameInfo = doc.getElementById(id).getElementsByClass("currency-name-container").outerHtml().replaceFirst("<", "");
+                        String priceInfo = doc.getElementById(id).getElementsByClass("price").outerHtml().replaceFirst("<", "");
+                        String capInfo = doc.getElementById(id).getElementsByClass("no-wrap market-cap text-right").outerHtml().replaceFirst("<", "");
+                        String volumeInfo = doc.getElementById(id).getElementsByClass("volume").outerHtml().replaceFirst("<", "");
+                        String supplyInfo = doc.getElementById(id).getElementsByClass("no-wrap text-right circulating-supply").outerHtml().replaceFirst("<", "");
+                        String changeInfo = doc.getElementById(id).outerHtml();
+
+                        changeInfo = changeInfo.substring(changeInfo.indexOf("data-percentusd"), changeInfo.indexOf("%<")+3);
+                        changeInfo = changeInfo.substring(changeInfo.indexOf(">")+1, changeInfo.indexOf("<"));
+
+                        capInfo = capInfo.substring(capInfo.indexOf(">") + 1, capInfo.indexOf("<"));
+                        capInfo = capInfo.trim();
+                        if(capInfo.contains("e"))
+                        {
+                            capInfo = capInfo.substring(0, capInfo.indexOf("e"));
+                            capInfo = capInfo.replace(".","");
+                        }
+                        NumberFormat myFormat = NumberFormat.getInstance();
+                        myFormat.setGroupingUsed(true);
+                        capInfo = "$" + myFormat.format(Double.parseDouble(capInfo));
+
+                        Log.d("factoid",  changeInfo);
                         coinInfo.add(new Coin(
-                                doc.getElementById(id).outerHtml(),id,"","",""));
+                                nameInfo.substring(nameInfo.indexOf(">") + 1, nameInfo.indexOf("<")),
+                                id,
+                                capInfo,
+                                priceInfo.substring(priceInfo.indexOf(">") + 1, priceInfo.indexOf("<")),
+                                volumeInfo.substring(volumeInfo.indexOf(">") + 1, volumeInfo.indexOf("<")),
+                                supplyInfo.substring(supplyInfo.indexOf("er>") + 3, supplyInfo.indexOf("</sp")),
+                                changeInfo));
+                       // Log.d("factoid", coinInfo.get(0).getDetails());
+                       // Log.d("factoid",  Integer.toString(nameInfo.substring(nameInfo.indexOf(">"), nameInfo.length()-1).indexOf("<")+nameInfo.indexOf(">")));
+                       // Log.d("factoid",  nameInfo.substring(nameInfo.indexOf(">") + 1, nameInfo.indexOf("<")));
+                    }
+                    for(Coin c : coinInfo)
+                    {
+                        Log.d("factoid",  c.getDetails());
                     }
 
 
